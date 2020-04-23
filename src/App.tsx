c3227986cb2d4af1aad50e7a974dabd44fce9ad7
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-23 15:31:58 -0700

    Route 2 separate pages: List, Create

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
 import List from './List';
+import Create from './Create';
 
 function App() {
-  return <List />;
+  return (
+    <Router>
+      <header>
+        <Link to="/">Home</Link> | <Link to="/create">Create</Link>
+      </header>
+      <Route exact path="/" component={List} />
+      <Route path="/create" component={Create} />
+    </Router>
+  );
 }
 
 export default App;
