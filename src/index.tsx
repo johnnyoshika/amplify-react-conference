4df4b70ec017757cf4847735d69546fa58f863ed
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-23 09:59:05 -0700

    Make React aware of Amplify

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
