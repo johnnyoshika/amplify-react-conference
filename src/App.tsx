6b773e056ffbd4307d055ac45f11a2f300437d59
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-23 11:24:16 -0700

    Set typescript base types for Talk

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+
+import { listTalks as ListTalks } from './graphql/queries';
+import { GetTalkQuery } from './API';
+
+interface GraphQLResult<T> {
+  data?: T;
+  errors?: [object];
+  extensions?: { [key: string]: any };
+}
+
+// https://dev.to/stevelizcano/5-minute-tutorial-get-base-types-from-your-aws-amplify-graphql-schema-with-typescript-3636
+interface Talk
+  extends Omit<
+    Exclude<GetTalkQuery['getTalk'], null>,
+    '__typename'
+  > {}
+
+// https://stackoverflow.com/a/59223775/188740
+type ListTalks = {
+  listTalks: {
+    items: Talk[];
+  };
+};
 
 function App() {
   return <div>Hello!</div>;
